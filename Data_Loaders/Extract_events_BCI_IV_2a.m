3dd629d32c814926e758f3f066011ec735d52041
Srirupa Datta <srirupa.data@outlook.com>
2023-06-24 13:18:04 +0530

    Add data extraction file

--- Data_Loaders/Extract_events_BCI_IV_2a.m
+++ Data_Loaders/Extract_events_BCI_IV_2a.m
@@ -1,12 +1,17 @@
 clear;
+
+% load dataset
 filename = 'A01T.gdf';
 [s, HDR] = sload(filename);
 
+% Extract EEG channels, discard EOG. First 22 channels are EEG
 s = s(:, 1:22);
 
 type=HDR.EVENT.TYP;
 pos=HDR.EVENT.POS;
 dur=HDR.EVENT.DUR;
+
+% Extract left, right, foot, tongue time segments
 iv_c1=1; iv_c2=1; iv_c3=1; iv_c4=1;
 for i=1:size(type,1)
     if type(i,1)==769
@@ -28,25 +33,24 @@ for i=1:size(type,1)
     end
 end
 
+% Create labels for dataset
 signals = [A01T_C1; A01T_C2; A01T_C3; A01T_C4];
 class1 = ones(size(A01T_C1, 1), 1);
 class2 = 2*ones(size(A01T_C2, 1), 1);
 class3 = 3*ones(size(A01T_C3, 1), 1);
 class4 = 4*ones(size(A01T_C4, 1), 1);
-
 labels = [class1;class2;class3;class4];
 
+% Shuffle the data and labels using the same indices to be used for training
 indices = randperm(size(signals, 1));
-
-% Shuffle the data and labels using the same indices
 shuffledData = signals(indices, :, :);
 shuffledLabels = labels(indices, :);
 
-data3 = struct('x', shuffledData, 'y', shuffledLabels);
+% Construct matlab struct
+data_struct = struct('x', shuffledData, 'y', shuffledLabels);
 
-
-
-json_data = jsonencode(data3);
-fid = fopen('data4.json', 'w');
+% Save as json
+json_data = jsonencode(data_struct);
+fid = fopen('data_A01T.json', 'w');
 fprintf(fid, '%s', json_data);
 fclose(fid);
